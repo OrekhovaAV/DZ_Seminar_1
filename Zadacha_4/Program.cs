11babcd264f8dfc62c1b8249689d266618b3a697
Orekhova Anastasia <Kopatulka1994@yandex.ru>
2022-10-04 23:11:29 +0300

    Решили вторую задачу

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел */
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int max = A;
+if (A>max) max = A;
+if (B>max) max = B;
+if (C>max) max = C;
+Console.Write("max = ");
+Console.WriteLine(max);
